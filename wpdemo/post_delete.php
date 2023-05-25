7462ee29f44031d38303d6f5b5209f157c260876
kaushal-yudiz <kaushal.gandhi@yudiz.com>
2023-05-25 19:11:29 +0530

    workig on delete

---
+++ wpdemo/post_delete.php
@@ -0,0 +1,14 @@
+<?php
+include 'config.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "DELETE FROM `post_table` WHERE `id` = '$id'";
+    $result = $conn->query($sql);
+    if ($result == TRUE) {
+        echo "<script>alert('Record deleted successfully')</script>";
+    } else {
+        echo "<script>alert('Something Went Wrong')</script>";
+    }
+    // header('location: view.php ');
+}
